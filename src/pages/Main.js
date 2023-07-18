916ba442c893e63f13d7324a1b7954913a119ccf
Ramirisu <labyrinth.ramirisu@gmail.com>
2023-07-18 11:35:15 +0800

    Correct indentation using 2 spaces.

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -6,36 +6,36 @@ const { Header, Content, Footer } = Layout
 const { Title } = Typography
 
 const Main = () => {
-    return (
-        <Layout style={{ minHeight: '100vh' }}>
-            <Header style=
-                {{
-                    display: 'flex',
-                    alignItems: 'center',
-                    padding: '20px 20% 40px 20%',
-                }}>
-                <Title level={1}>
-                    TW Values
-                </Title>
-            </Header>
-            <Content style=
-                {{
-                    display: 'flex',
-                    alignItems: 'center',
-                    padding: '10px 20% 10px 20%',
-                    backgroundColor: '#E0E0E0',
-                }}>
-                <Outlet />
-            </Content>
-            <Footer style=
-                {{
-                    padding: '0 20% 0 20%',
-                    textAlign: 'right',
-                }}>
-                <Title level={4}>Copyright (c) 2023 TW Values</Title>
-            </Footer >
-        </Layout >
-    )
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Header style=
+        {{
+          display: 'flex',
+          alignItems: 'center',
+          padding: '20px 20% 40px 20%',
+        }}>
+        <Title level={1}>
+          TW Values
+        </Title>
+      </Header>
+      <Content style=
+        {{
+          display: 'flex',
+          alignItems: 'center',
+          padding: '10px 20% 10px 20%',
+          backgroundColor: '#E0E0E0',
+        }}>
+        <Outlet />
+      </Content>
+      <Footer style=
+        {{
+          padding: '0 20% 0 20%',
+          textAlign: 'right',
+        }}>
+        <Title level={4}>Copyright (c) 2023 TW Values</Title>
+      </Footer >
+    </Layout >
+  )
 }
 
 export default Main
--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -3,105 +3,105 @@ import { useState } from "react"
 
 const Quiz = () => {
 
-    const [questionIdx, setQuestionIdx] = useState(0)
+  const [questionIdx, setQuestionIdx] = useState(0)
 
-    const questions = [
-        {
-            id: 1,
-            question: 'The freer the markets, the freer the people.',
-            effect: {
-                econamic: -10,
-                deplomatic: 0,
-                civil: 0,
-                societal: 0,
-            }
-        },
-    ]
+  const questions = [
+    {
+      id: 1,
+      question: 'The freer the markets, the freer the people.',
+      effect: {
+        econamic: -10,
+        deplomatic: 0,
+        civil: 0,
+        societal: 0,
+      }
+    },
+  ]
 
-    return (
-        <Layout style={{
-            backgroundColor: 'transparent',
-            display: 'flex',
-            alignItems: 'center',
+  return (
+    <Layout style={{
+      backgroundColor: 'transparent',
+      display: 'flex',
+      alignItems: 'center',
+    }}>
+      <Card
+        title={`Question ${questionIdx + 1} of ${questions.length}`}
+        headStyle={{
+          fontSize: 'xx-large',
+        }}
+        style={{
+          width: '100%',
+          fontSize: 'x-large',
+          margin: '20px',
         }}>
-            <Card
-                title={`Question ${questionIdx + 1} of ${questions.length}`}
-                headStyle={{
-                    fontSize: 'xx-large',
-                }}
-                style={{
-                    width: '100%',
-                    fontSize: 'x-large',
-                    margin: '20px',
-                }}>
-                {questions[questionIdx].question}
-            </Card>
-            <Button style={{
-                backgroundColor: '#006000',
-                borderColor: '#006000',
-                color: 'white',
-                width: '60%',
-                height: '40px',
-                margin: '5px',
-                fontSize: 'large',
-            }}>
-                Strongly Agree
-            </Button>
-            <Button style={{
-                backgroundColor: '#00D000',
-                borderColor: '#00D000',
-                color: 'white',
-                width: '60%',
-                height: '40px',
-                margin: '5px',
-                fontSize: 'large',
-            }}>
-                Agree
-            </Button>
-            <Button style={{
-                backgroundColor: 'gray',
-                borderColor: 'gray',
-                color: 'white',
-                width: '60%',
-                height: '40px',
-                margin: '5px',
-                fontSize: 'large',
-            }}>
-                Neutral
-            </Button>
-            <Button style={{
-                backgroundColor: 'red',
-                borderColor: 'red',
-                color: 'white',
-                width: '60%',
-                height: '40px',
-                margin: '5px',
-                fontSize: 'large',
-            }}>
-                Disagree
-            </Button>
-            <Button style={{
-                backgroundColor: '#600000',
-                borderColor: '#600000',
-                color: 'white',
-                width: '60%',
-                height: '40px',
-                margin: '5px',
-                fontSize: 'large',
-            }}>
-                Strongly Disagree
-            </Button>
-            <Button style={{
-                backgroundColor: 'transparent',
-                borderColor: 'gray',
-                color: 'gray',
-                width: '20%',
-                margin: '5px',
-            }}>
-                Back
-            </Button>
-        </Layout >
-    )
+        {questions[questionIdx].question}
+      </Card>
+      <Button style={{
+        backgroundColor: '#006000',
+        borderColor: '#006000',
+        color: 'white',
+        width: '60%',
+        height: '40px',
+        margin: '5px',
+        fontSize: 'large',
+      }}>
+        Strongly Agree
+      </Button>
+      <Button style={{
+        backgroundColor: '#00D000',
+        borderColor: '#00D000',
+        color: 'white',
+        width: '60%',
+        height: '40px',
+        margin: '5px',
+        fontSize: 'large',
+      }}>
+        Agree
+      </Button>
+      <Button style={{
+        backgroundColor: 'gray',
+        borderColor: 'gray',
+        color: 'white',
+        width: '60%',
+        height: '40px',
+        margin: '5px',
+        fontSize: 'large',
+      }}>
+        Neutral
+      </Button>
+      <Button style={{
+        backgroundColor: 'red',
+        borderColor: 'red',
+        color: 'white',
+        width: '60%',
+        height: '40px',
+        margin: '5px',
+        fontSize: 'large',
+      }}>
+        Disagree
+      </Button>
+      <Button style={{
+        backgroundColor: '#600000',
+        borderColor: '#600000',
+        color: 'white',
+        width: '60%',
+        height: '40px',
+        margin: '5px',
+        fontSize: 'large',
+      }}>
+        Strongly Disagree
+      </Button>
+      <Button style={{
+        backgroundColor: 'transparent',
+        borderColor: 'gray',
+        color: 'gray',
+        width: '20%',
+        margin: '5px',
+      }}>
+        Back
+      </Button>
+    </Layout >
+  )
 }
 
 export default Quiz
