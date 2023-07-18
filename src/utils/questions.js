5bed22abd9ef7c181ed1a7a57408b5f2c2a73b90
Ramirisu <labyrinth.ramirisu@gmail.com>
2023-07-18 23:13:37 +0800

    Indent using 2 spaces.

--- src/utils/questions.js
+++ src/utils/questions.js
@@ -1,125 +1,125 @@
 
 const QUESTIONS = [
-    {
-        id: "q0000",
-        effect: {
-            econamic: 10,
-            deplomatic: 0,
-            civil: 0,
-            societal: 0,
-        }
-    },
-    {
-        id: "q0001",
-        effect: {
-            econamic: 10,
-            deplomatic: 0,
-            civil: 0,
-            societal: 0,
-        }
-    },
-    {
-        id: "q0002",
-        effect: {
-            econamic: 10,
-            deplomatic: 0,
-            civil: 0,
-            societal: 0,
-        }
-    },
-    {
-        id: "q0003",
-        effect: {
-            econamic: 10,
-            deplomatic: 0,
-            civil: 0,
-            societal: 0,
-        }
-    },
-    {
-        id: "q0100",
-        effect: {
-            econamic: -10,
-            deplomatic: 0,
-            civil: 0,
-            societal: 0,
-        }
-    },
-    {
-        id: "q0101",
-        effect: {
-            econamic: -10,
-            deplomatic: 0,
-            civil: 0,
-            societal: 0,
-        }
-    },
-    {
-        id: "q0102",
-        effect: {
-            econamic: -10,
-            deplomatic: 0,
-            civil: 0,
-            societal: 0,
-        }
-    },
-    {
-        id: "q0103",
-        effect: {
-            econamic: -10,
-            deplomatic: 0,
-            civil: 0,
-            societal: 0,
-        }
-    },
-    {
-        id: "q1000",
-        effect: {
-            sovereignty: 10,
-        }
-    },
-    {
-        id: "q1001",
-        effect: {
-            sovereignty: 10,
-        }
-    },
-    {
-        id: "q1100",
-        effect: {
-            sovereignty: -10,
-        }
-    },
-    {
-        id: "q1101",
-        effect: {
-            sovereignty: -10,
-        }
-    },
-    {
-        id: "q1200",
-        effect: {
-            pro_usa: 10,
-        }
-    },
-    {
-        id: "q1201",
-        effect: {
-            pro_usa: 10,
-        }
-    },
-    {
-        id: "q1300",
-        effect: {
-            pro_usa: -10,
-        }
-    },
-    {
-        id: "q1301",
-        effect: {
-            pro_usa: -10,
-        }
-    },
+  {
+    id: "q0000",
+    effect: {
+      econamic: 10,
+      deplomatic: 0,
+      civil: 0,
+      societal: 0,
+    }
+  },
+  {
+    id: "q0001",
+    effect: {
+      econamic: 10,
+      deplomatic: 0,
+      civil: 0,
+      societal: 0,
+    }
+  },
+  {
+    id: "q0002",
+    effect: {
+      econamic: 10,
+      deplomatic: 0,
+      civil: 0,
+      societal: 0,
+    }
+  },
+  {
+    id: "q0003",
+    effect: {
+      econamic: 10,
+      deplomatic: 0,
+      civil: 0,
+      societal: 0,
+    }
+  },
+  {
+    id: "q0100",
+    effect: {
+      econamic: -10,
+      deplomatic: 0,
+      civil: 0,
+      societal: 0,
+    }
+  },
+  {
+    id: "q0101",
+    effect: {
+      econamic: -10,
+      deplomatic: 0,
+      civil: 0,
+      societal: 0,
+    }
+  },
+  {
+    id: "q0102",
+    effect: {
+      econamic: -10,
+      deplomatic: 0,
+      civil: 0,
+      societal: 0,
+    }
+  },
+  {
+    id: "q0103",
+    effect: {
+      econamic: -10,
+      deplomatic: 0,
+      civil: 0,
+      societal: 0,
+    }
+  },
+  {
+    id: "q1000",
+    effect: {
+      sovereignty: 10,
+    }
+  },
+  {
+    id: "q1001",
+    effect: {
+      sovereignty: 10,
+    }
+  },
+  {
+    id: "q1100",
+    effect: {
+      sovereignty: -10,
+    }
+  },
+  {
+    id: "q1101",
+    effect: {
+      sovereignty: -10,
+    }
+  },
+  {
+    id: "q1200",
+    effect: {
+      pro_usa: 10,
+    }
+  },
+  {
+    id: "q1201",
+    effect: {
+      pro_usa: 10,
+    }
+  },
+  {
+    id: "q1300",
+    effect: {
+      pro_usa: -10,
+    }
+  },
+  {
+    id: "q1301",
+    effect: {
+      pro_usa: -10,
+    }
+  },
 ]
 
 export default QUESTIONS
