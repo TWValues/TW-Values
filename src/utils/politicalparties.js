58c89cae1c975cbea5345a8e138b02f63f19746a
Ramirisu <labyrinth.ramirisu@gmail.com>
2023-07-21 12:03:20 +0800

    Indent using 2 spaces.

--- src/utils/politicalparties.js
+++ src/utils/politicalparties.js
@@ -8,102 +8,102 @@ import CUPPIcon from '../assets/parties/CUPPIcon.svg'
 import SDPIcon from '../assets/parties/SDPIcon.svg'
 
 const POLITICAL_PARTIES = [
-    {
-        id: "kmt",
-        icon: KMTIcon,
-        state: {
-            economic: 40,
-            environmental: 40,
-            civil: 35,
-            societal: 30,
-            sovereignty: 40,
-            us_china_relation: 35,
-        }
-    },
-    {
-        id: "dpp",
-        icon: DPPIcon,
-        state: {
-            economic: 50,
-            environmental: 60,
-            civil: 50,
-            societal: 45,
-            sovereignty: 65,
-            us_china_relation: 70,
-        }
-    },
-    {
-        id: "np",
-        icon: NPIcon,
-        state: {
-            economic: 60,
-            environmental: 40,
-            civil: 35,
-            societal: 20,
-            sovereignty: 20,
-            us_china_relation: 20,
-        }
-    },
-    {
-        id: "npp",
-        icon: NPPIcon,
-        state: {
-            economic: 75,
-            environmental: 75,
-            civil: 75,
-            societal: 75,
-            sovereignty: 75,
-            us_china_relation: 70,
-        }
-    },
-    {
-        id: "tmd",
-        icon: TMDIcon,
-        state: {
-            economic: 50,
-            environmental: 50,
-            civil: 45,
-            societal: 40,
-            sovereignty: 45,
-            us_china_relation: 40,
-        }
-    },
-    {
-        id: "tsp",
-        icon: TSPIcon,
-        state: {
-            economic: 60,
-            environmental: 50,
-            civil: 75,
-            societal: 50,
-            sovereignty: 90,
-            us_china_relation: 80,
-        }
-    },
-    {
-        id: "cupp",
-        icon: CUPPIcon,
-        state: {
-            economic: 80,
-            environmental: 20,
-            civil: 10,
-            societal: 20,
-            sovereignty: 10,
-            us_china_relation: 10,
-        }
-    },
-    {
-        id: "sdp",
-        icon: SDPIcon,
-        state: {
-            economic: 80,
-            environmental: 75,
-            civil: 75,
-            societal: 80,
-            sovereignty: 65,
-            us_china_relation: 70,
-        }
-    },
+  {
+    id: "kmt",
+    icon: KMTIcon,
+    state: {
+      economic: 40,
+      environmental: 40,
+      civil: 35,
+      societal: 30,
+      sovereignty: 40,
+      us_china_relation: 35,
+    }
+  },
+  {
+    id: "dpp",
+    icon: DPPIcon,
+    state: {
+      economic: 50,
+      environmental: 60,
+      civil: 50,
+      societal: 45,
+      sovereignty: 65,
+      us_china_relation: 70,
+    }
+  },
+  {
+    id: "np",
+    icon: NPIcon,
+    state: {
+      economic: 60,
+      environmental: 40,
+      civil: 35,
+      societal: 20,
+      sovereignty: 20,
+      us_china_relation: 20,
+    }
+  },
+  {
+    id: "npp",
+    icon: NPPIcon,
+    state: {
+      economic: 75,
+      environmental: 75,
+      civil: 75,
+      societal: 75,
+      sovereignty: 75,
+      us_china_relation: 70,
+    }
+  },
+  {
+    id: "tmd",
+    icon: TMDIcon,
+    state: {
+      economic: 50,
+      environmental: 50,
+      civil: 45,
+      societal: 40,
+      sovereignty: 45,
+      us_china_relation: 40,
+    }
+  },
+  {
+    id: "tsp",
+    icon: TSPIcon,
+    state: {
+      economic: 60,
+      environmental: 50,
+      civil: 75,
+      societal: 50,
+      sovereignty: 90,
+      us_china_relation: 80,
+    }
+  },
+  {
+    id: "cupp",
+    icon: CUPPIcon,
+    state: {
+      economic: 80,
+      environmental: 20,
+      civil: 10,
+      societal: 20,
+      sovereignty: 10,
+      us_china_relation: 10,
+    }
+  },
+  {
+    id: "sdp",
+    icon: SDPIcon,
+    state: {
+      economic: 80,
+      environmental: 75,
+      civil: 75,
+      societal: 80,
+      sovereignty: 65,
+      us_china_relation: 70,
+    }
+  },
 ]
 
 export default POLITICAL_PARTIES
