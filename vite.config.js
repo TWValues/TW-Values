d8074d439520cca671ccd21d4e767e8a9e5a7789
Ramirisu <labyrinth.ramirisu@gmail.com>
2024-01-15 02:39:36 +0800

    Add missing base path for github pages deployment.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+    base: '/TW-Values',
     plugins: [react(), viteTsconfigPaths()],
     server: {
         open: true,
