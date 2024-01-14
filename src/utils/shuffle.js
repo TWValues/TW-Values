1fbdd479f6d56134bb07129b707c257ef3107cd3
Ramirisu <labyrinth.ramirisu@gmail.com>
2024-01-15 02:20:44 +0800

    Fix `shuffle` by removing buggy multeple assignment feature from ES6.

--- src/utils/shuffle.js
+++ src/utils/shuffle.js
@@ -1,17 +1,13 @@
-// https://stackoverflow.com/a/2450976
 const shuffle = (array) => {
-    let currentIndex = array.length, randomIndex
+    for (let i = 0; i < array.length; ++i) {
 
-    // While there remain elements to shuffle.
-    while (currentIndex !== 0) {
+        let idx = Math.floor(Math.random() * array.length)
+        idx = Math.min(idx, array.length - 1)
+        idx = Math.max(idx, 0)
 
-        // Pick a remaining element.
-        randomIndex = Math.floor(Math.random() * currentIndex)
-        currentIndex--
-
-        // And swap it with the current element.
-        [array[currentIndex], array[randomIndex]] = [
-            array[randomIndex], array[currentIndex]]
+        let temp = array[i]
+        array[i] = array[idx]
+        array[idx] = temp
     }
 
     return array
