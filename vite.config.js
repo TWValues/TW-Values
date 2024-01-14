0a9e501022f02f087de7e34f703d2a8ad6cacb06
Ramirisu <labyrinth.ramirisu@gmail.com>
2024-01-15 02:24:55 +0800

    Migrate from `react-scripts` to `vite`.

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+    plugins: [react(), viteTsconfigPaths()],
+    server: {
+        open: true,
+        port: 3000,
+    },
+})
