ea0e16808fb50a8531df43a7a2bee2250cafa6f0
Ramirisu <labyrinth.ramirisu@gmail.com>
2024-02-02 21:07:29 +0800

    Format code.

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-    base: '/TW-Values',
-    plugins: [react(), viteTsconfigPaths()],
-    server: {
-        open: true,
-        port: 3000,
-    },
+  base: '/TW-Values',
+  plugins: [react(), viteTsconfigPaths()],
+  server: {
+    open: true,
+    port: 3000,
+  },
 })
